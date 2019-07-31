73119e0e3e7dde219ba2730b6c5ffd8781dd5d2d
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-07-30 23:25:17 -0300

    version 1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:agenda_contatos/pages/contact_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:agenda_contatos/pages/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
